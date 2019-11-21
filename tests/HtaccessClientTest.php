c881a6a3bae1ab79b01792446c1a39ae8c3a81f6
woutersioen <wouter.sioen@gmail.com>
2019-11-21 11:04:56 +0100

    Expect us to be able to pass a server name

--- tests/HtaccessClientTest.php
+++ tests/HtaccessClientTest.php
@@ -60,4 +60,26 @@ final class HtaccessClientTest extends TestCase
             $response->getOutputUrl()
         );
     }
+
+    /** @test */
+    public function it allows for passing a server name(): void
+    {
+        $client = new HtaccessClient(
+            new Client(),
+            new ServerRequestFactory()
+        );
+
+        $response = $client->test(
+            'http://localhost',
+            'RewriteCond %{SERVER_NAME} example.com
+             RewriteRule .* /example-page [L]',
+            null,
+            'example.com'
+        );
+
+        $this->assertEquals(
+            'http://localhost/example-page',
+            $response->getOutputUrl()
+        );
+    }
 }
