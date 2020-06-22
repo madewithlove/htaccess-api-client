5a0dc0976d7defb11e6b24fc104089153fc940c6
woutersioen <wouter.sioen@gmail.com>
2020-06-22 10:59:35 +0200

    Expect sharing with a server name to work
    Note that this does not pass tests at the moment because of a bug this
    uncovered, nice!

--- tests/HtaccessClientTest.php
+++ tests/HtaccessClientTest.php
@@ -196,4 +196,38 @@ final class HtaccessClientTest extends TestCase
             $sharedResult->getOutputUrl()
         );
     }
+
+    /** @test */
+    public function it can share a test run with a server name(): void
+    {
+        $client = new HtaccessClient(
+            new Client(),
+            new ServerRequestFactory()
+        );
+
+        $response = $client->share(
+            'http://localhost',
+            'RewriteCond %{SERVER_NAME} example.com
+             RewriteRule .* /example-page [L]',
+            null,
+            'example.com'
+        );
+
+        $this->assertStringStartsWith(
+            'https://htaccess.madewithlove.be',
+            $response->getShareUrl()
+        );
+        $this->assertRegExp(
+            '#.*?share=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}#',
+            $response->getShareUrl()
+        );
+
+        $shareUuid = substr($response->getShareUrl(), -36);
+        $sharedResult = $client->getShared($shareUuid);
+
+        $this->assertEquals(
+            'http://localhost/example-page',
+            $sharedResult->getOutputUrl()
+        );
+    }
 }
