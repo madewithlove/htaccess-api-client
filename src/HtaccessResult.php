347aacf93b12a5bfd28d4dde35b329a53585f8ba
woutersioen <wouter.sioen@gmail.com>
2020-12-08 17:24:50 +0100

    Add output status code to the result data value object

--- src/HtaccessResult.php
+++ src/HtaccessResult.php
@@ -14,9 +14,18 @@ final class HtaccessResult
      */
     private $lines = [];
 
-    public function __construct(string $outputUrl, array $lines)
-    {
+    /**
+     * @var int?
+     */
+    private $outputStatusCode;
+
+    public function __construct(
+        string $outputUrl,
+        array $lines,
+        ?int $outputStatusCode
+    ) {
         $this->outputUrl = $outputUrl;
+        $this->outputStatusCode = $outputStatusCode;
 
         foreach ($lines as $line) {
             $this->addLine($line);
@@ -34,6 +43,11 @@ final class HtaccessResult
     public function getLines(): array
     {
         return $this->lines;
+    }
+
+    public function getOutputStatusCode(): ?int
+    {
+        return $this->outputStatusCode;
     }
 
     private function addLine(ResultLine $line): void
